b40e6227ace182bb365876ab9c6783a5f43866de
thapasijan17 <thapasijan329@gmail.com>
2025-01-06 20:58:37 +0545

    feat: added stater site

---
+++ src/app/resources/content.js
@@ -0,0 +1,17 @@
+import { InlineCode } from "@/once-ui/components";
+
+const content = {
+    name: 'Sijan Thapa',
+    role: 'SDE',
+    home: {
+        headline: <>Hello tesh 123</>,
+        subline: <>I'm Sijan, SDE engineer at <InlineCode>❖ XYZ</InlineCode>, working on xyz projects.</>
+    },
+    social: {
+        github: 'https://github.com/thapasijan17',
+        linkedin: 'https://www.linkedin.com/sijanthapa/',
+        x: 'https://www.x.com/thapasijan171',
+    }
+}
+
+export { content };
